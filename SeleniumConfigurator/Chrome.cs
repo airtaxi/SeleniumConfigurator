e0229c3e7cd8d88303fb562b4ec914cc9a9b9320
Howon Lee <kck4156@kagamine-rin.com>
2022-09-14 20:54:35 +0900

    Add project files.

---
+++ SeleniumConfigurator/Chrome.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumConfigurator;
+
+public static class Chrome
+{
+    private static string GetVersion()
+    {
+        try
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Google\Chrome\BLBeacon", true);
+            string version = key.GetValue("version") as string;
+            return version;
+        }
+        catch (Exception) { return null; }
+    }
+
+    public static string GetDriverPath()
+    {
+        var version = GetVersion();
+        if (version == null) throw new Exception("Google Chrome Not Installed");
+        var driverPath = new DriverManager().SetUpDriver(new ChromeConfig(), version);
+        return driverPath;
+    }
+}
---
+++ SeleniumConfigurator/Edge.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumConfigurator;
+
+public static class Edge
+{
+    private static string GetVersion()
+    {
+        try
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Edge\BLBeacon", true);
+            string version = key.GetValue("version") as string;
+            return version;
+        }
+        catch (Exception) { return null; }
+    }
+
+    public static string GetDriverPath()
+    {
+        var version = GetVersion();
+        if (version == null) throw new Exception("Microsoft Edge Not Installed");
+        var driverPath = new DriverManager().SetUpDriver(new EdgeConfig(), version);
+        return driverPath;
+    }
+}
